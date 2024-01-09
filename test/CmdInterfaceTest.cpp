de62b8b37faf154882b6bbd5dab2adca1cfa1b80
Rockdu <kangruidu@163.com>
2024-01-09 15:10:02 +0800

    ZipFileListTest added

--- test/CmdInterfaceTest.cpp
+++ test/CmdInterfaceTest.cpp
@@ -1,67 +1,37 @@
 #include <gtest/gtest.h>
-#include "ZipFileList.h" 
+#include "CmdInterface.h" 
 #include <vector>
 
-// 测试 str_to_vec 函数
-TEST(StrToVecTest, HandlesEmptyString) {
-    std::string empty_str = "";
-    std::vector<char> result = str_to_vec(empty_str);
-    EXPECT_TRUE(result.empty());
-}
-
-TEST(StrToVecTest, HandlesNonEmptyString) {
-    std::string str = "test";
-    std::vector<char> result = str_to_vec(str);
-    ASSERT_EQ(result.size(), str.size());
-    for (size_t i = 0; i < str.size(); ++i) {
-        EXPECT_EQ(result[i], str[i]);
-    }
-}
-
-// 测试 vec_to_string 函数
-TEST(VecToStringTest, HandlesEmptyVector) {
-    std::vector<char> empty_vec;
-    std::string result = vec_to_string(empty_vec);
-    EXPECT_TRUE(result.empty());
-}
-
-TEST(VecToStringTest, HandlesNonEmptyVector) {
-    std::vector<char> vec = {'t', 'e', 's', 't'};
-    std::string result = vec_to_string(vec);
-    EXPECT_EQ(result, "test");
-}
-
-class ZipFileListTest : public ::testing::Test {
-protected:
-    ZipFileList zfl;
-
-    void SetUp() override {
-       zfl.zipAccessor = new ZipAccessor;
-    }
-
-    void TearDown() override {
-       zfl.zipAccessor = new ZipAccessor;
-    }
+struct SplitStringTestParam {
+    std::string input;
+    std::string expected_part1;
+    std::string expected_part2;
 };
 
-TEST_F(ZipFileListTest, SetPasswordNoOldPassword) {
-    EXPECT_EQ(zfl.set_password("newpassword"), 1);
+class SplitStringTest : public ::testing::TestWithParam<SplitStringTestParam> {
+
+};
+
+TEST_P(SplitStringTest, SplitsCorrectly) {
+    SplitStringTestParam const& param = GetParam();
+    std::string part1, part2;
+    CmdInterface::splitString(param.input, part1, part2);
+    EXPECT_EQ(part1, param.expected_part1);
+    EXPECT_EQ(part2, param.expected_part2);
 }
 
-TEST_F(ZipFileListTest, SetPasswordWithOldPassword) {
-    zfl.set_password("oldpassword");
-    EXPECT_EQ(zfl.set_password("newpassword"), 0);
-}
-
-TEST_F(ZipFileListTest, ResetPasswordCorrectOldPassword) {
-    zfl.set_password("oldpassword");
-    EXPECT_EQ(zfl.reset_password("oldpassword"), 1);
-}
-
-TEST_F(ZipFileListTest, ResetPasswordWrongOldPassword) {
-    zfl.set_password("oldpassword");
-    EXPECT_EQ(zfl.reset_password("wrongpassword"), 0);
-}
+INSTANTIATE_TEST_SUITE_P(
+    Splits,
+    SplitStringTest,
+    ::testing::Values(
+        SplitStringTestParam{"test-string", "test", "string"},
+        SplitStringTestParam{"no-dash", "no", "dash"},
+        SplitStringTestParam{"multiple-dashes-string", "multiple", "dashes-string"},
+        SplitStringTestParam{"-", "", ""},
+        SplitStringTestParam{"", "", ""}
+        // 更多测试用例
+    )
+);
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
